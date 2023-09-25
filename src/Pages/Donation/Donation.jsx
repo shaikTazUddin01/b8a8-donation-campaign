1dda16bcbacbb7071595c44c89ba81a03ed4d225
sheikhtazuddin <tazahmedsoft@gmail.com>
2023-09-26 02:04:16 +0600

    added scarch bar to scarch category

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -2,27 +2,35 @@ import React, { useEffect, useState } from 'react';
 import ShowDonation from './ShowDonation';
 
 const Donation = () => {
-    const [getDonation,setGetDonaton]=useState([]);
-    const[showbutton,setShowButton]=useState(false)
+    const [getDonation, setGetDonaton] = useState([]);
+    const [showMore, setShowMore] = useState(false)
 
 
-    useEffect(()=>{
-        const getDonationByStroage=localStorage.getItem("donation")
+    useEffect(() => {
+        const getDonationByStroage = localStorage.getItem("donation")
         setGetDonaton(JSON.parse(getDonationByStroage));
-    },[])
-    
-    const handleShow=()=>{}
+    }, [])
+
+    const handleShow = () => {
+        setShowMore(true)
+    }
     return (
-       <div>
-         <div className='grid grid-cols-2 gap-5 max-w-7xl mx-auto'>
-            {
-                getDonation.map(donation=><ShowDonation key={donation.id} donation={donation}></ShowDonation>)
-            }
+        <div className='mt-20'>
+            <div className='grid grid-cols-2 gap-5 max-w-7xl mx-auto'>
+                {
+                    (!showMore) ?
+                        getDonation.slice(0, 4).map(donation => <ShowDonation key={donation.id} donation={donation}></ShowDonation>)
+                        : getDonation.map(donation => <ShowDonation key={donation.id} donation={donation}></ShowDonation>)
+                }
+            </div>
+            <div className='flex justify-center my-10'>
+                {
+
+                    getDonation.length >= 4 && !showMore &&
+                    <button className='btn btn-primary' onClick={handleShow}>Show All</button>
+                }
+            </div>
         </div>
-        <div className='flex justify-center mt-10'>
-            <button className='btn btn-primary' onClick={handleShow}>Show All</button>
-        </div>
-       </div>
     );
 };
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,29 @@
 import { useLoaderData } from 'react-router-dom';
 import Donations from '../../Component/Home/Donations/Donations';
 import './home.css'
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+import { useRef, useState } from 'react';
 const Home = () => {
-    const donations=useLoaderData()
+    const donations = useLoaderData();
+
+    const [category,setCategory]=useState([]);
+
+    const scarch=useRef();
+    const handlescarch=()=>{
+        const getScarchValue=scarch.current.value;
+
+        // setScarchvalue(getScarchValue)
+        const filterData=donations.filter(data=>data.Category===getScarchValue)
+        setCategory(filterData)
+        if (category.length===0) {
+           toast.error("No Data found")
+        }
+        console.log(filterData)
+    }
+    
+    
+
     return (
         <div>
             <section className='bannerSection w-full'>
@@ -10,11 +31,11 @@ const Home = () => {
                     <div className="hero-overlay banner-overlay"></div>
                     <div className="hero-content text-center text-neutral-content">
                         <div className="home-text">
-                        <h1 className="mb-5 text-5xl font-bold">I Grow By Helping People In Need</h1>
+                            <h1 className="mb-5 text-5xl font-bold">I Grow By Helping People In Need</h1>
                             <div className='mt-10'>
-                            <input type="text" className="w-2/3 p-3 rounded-l border" placeholder="scarch here..." />
-                            <button className="bg-[#FF444A] text-white py-3 px-5 text-[16px] font-semibold rounded-r ">Scarch</button>
-                            {/* <button className='bg-[#FF444A] text-white p-3 rounded-r '>search</button> */}
+                                <input type="text" ref={scarch} className="w-2/3 p-3 rounded-l border" placeholder="scarch here..." />
+                                <button className="bg-[#FF444A] text-white py-3 px-5 text-[16px] font-semibold rounded-r " onClick={handlescarch}>Scarch</button>
+                                {/* <button className='bg-[#FF444A] text-white p-3 rounded-r '>search</button> */}
                             </div>
                         </div>
                     </div>
@@ -22,8 +43,13 @@ const Home = () => {
             </section>
             <section className='max-w-7xl mx-auto mt-20'>
                 {/* <h1>{donations.length}</h1> */}
-                <Donations donations={donations}></Donations>
+                {
+                    Array.isArray(category) && category.length>0?<Donations donations={category}></Donations>
+                    :<Donations donations={donations}></Donations>
+                }
+                
             </section>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
