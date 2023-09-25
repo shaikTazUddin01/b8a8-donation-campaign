d7fe19aeca612ceb3120dd4169d2ef36364f3e73
sheikhtazuddin <tazahmedsoft@gmail.com>
2023-09-25 23:46:36 +0600

    added all donation card and show specific data for the card

--- src/Pages/DonationDetails/DonationDetails.jsx
+++ src/Pages/DonationDetails/DonationDetails.jsx
@@ -2,31 +2,31 @@ import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 // import './donationDetails.css'
 const DonationDetails = () => {
-    // const donations = useLoaderData()
-    // const { id } = useParams()
-    // const idInt = parseInt(id)
-    // const [donationDetail, setDonationDetail] = useState([])
+    const donations = useLoaderData()
+    const { id } = useParams()
+    const idInt = parseInt(id)
+    const [donationDetail, setDonationDetail] = useState([])
 
-    // useEffect(() => {
-    //     const donation = donations.find(item => item.id == idInt)
-    //     setDonationDetail(donation)
-    // }, [donations, id])
+    useEffect(() => {
+        const donation = donations.find(item => item.id == idInt)
+        setDonationDetail(donation)
+    }, [donations, id])
 
 
-    // return (
-    //     <div className='max-w-6xl mx-auto my-20'>
-    //         <div className=' max-w-4xl mx-auto relative donation'>
-    //             <img src={donationDetail.Picture} alt="" className='w-full h-auto mx-auto z-0' />
-    //             <div className='bg-red-400 z-10 -mt-[88px] p-5 donation-button'>
-    //                 <button className='btn btn-error text-white opacity-200'>Donate ${donationDetail.Price}</button>
-    //             </div>
-    //         </div>
-    //         <div className='max-w-4xl mx-auto'>
-    //             <h1 className='text-4xl font-bold mt-5'>{donationDetail.Title}</h1>
-    //             <p className='text-[16px] font-normal mt-2'>{donationDetail.Description}</p>
-    //         </div>
-    //     </div>
-    // );
+    return (
+        <div className='max-w-6xl mx-auto my-20'>
+            <div className=' max-w-4xl mx-auto relative donation'>
+                <img src={donationDetail.Picture} alt="" className='w-full h-auto mx-auto z-0' />
+                <div className='bg-[#1d1d1c94] -mt-[88px] p-5 donation-button relative'>
+                    <button className='btn btn-error text-white opacity-200'>Donate ${donationDetail.Price}</button>
+                </div>
+            </div>
+            <div className='max-w-4xl mx-auto'>
+                <h1 className='text-4xl font-bold mt-5'>{donationDetail.Title}</h1>
+                <p className='text-[16px] font-normal mt-2'>{donationDetail.Description}</p>
+            </div>
+        </div>
+    );
 };
 
 export default DonationDetails;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,10 +2,28 @@ import { useLoaderData } from 'react-router-dom';
 import Donations from '../../Component/Home/Donations/Donations';
 import './home.css'
 const Home = () => {
-    // const donations=useLoaderData()
+    const donations=useLoaderData()
     return (
         <div>
-            
+            <section className='bannerSection w-full'>
+                <div className="hero min-h-[400px] banner" >
+                    <div className="hero-overlay banner-overlay"></div>
+                    <div className="hero-content text-center text-neutral-content">
+                        <div className="home-text">
+                        <h1 className="mb-5 text-5xl font-bold">I Grow By Helping People In Need</h1>
+                            <div className='mt-10'>
+                            <input type="text" className="w-2/3 p-3 rounded-l border" placeholder="scarch here..." />
+                            <button className="bg-[#FF444A] text-white py-3 px-5 text-[16px] font-semibold rounded-r ">Scarch</button>
+                            {/* <button className='bg-[#FF444A] text-white p-3 rounded-r '>search</button> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='max-w-7xl mx-auto mt-20'>
+                {/* <h1>{donations.length}</h1> */}
+                <Donations donations={donations}></Donations>
+            </section>
         </div>
     );
 };
